3affe5934a831010602e9fae00a9816e07998d9e
BrunoPdOliveir4 <bpedrosodroliveira10@gmail.com>
2025-04-30 23:35:23 -0300

    social login

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,10 +1,6 @@
 const container = document.getElementById('container');
 const loginButton = document.getElementById('login-button');
 const errorMessage = document.getElementById('error-message');
-const id = '7lc5yxkxyhpj7m1wuopfr33yp8f0ya'; 
-const redirectUri = 'https://obs-interactivescreen.onrender.com/profile'; 
-
-const loginUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${id}&redirect_uri=${redirectUri}&response_type=code&scope=user:read:email`;
 
 loginButton.addEventListener('click', () => {
     window.location.href = loginUrl;  
@@ -12,24 +8,42 @@ loginButton.addEventListener('click', () => {
 
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
+fetch('/get-oauth-info')
+      .then(response => response.json())
+      .then(data => {
+        const clientId = data.clientId;
+        const redirectUri = data.redirectUri;
+        
+        const loginUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=user:read:email`;
+        
+        loginButton.addEventListener('click', () => {
+            window.location.href = loginUrl;
+        });
+        
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code');
 
-if (code) {
-    fetch('/get-token', {
-    method: 'POST',
-    body: JSON.stringify({ code }),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-    })
-    .then(response => response.json())
-    .then(data => {
-    if (data.access_token) {
-        container.innerHTML = `<p>Bem-vindo, seu login foi bem-sucedido! Acesso concedido com o token: ${data.access_token}</p>`;
-    } else {
-        errorMessage.textContent = 'Falha ao obter o token de acesso.';
-    }
-    })
-    .catch(err => {
-    errorMessage.textContent = 'Erro de conexão. Tente novamente mais tarde.';
-    });
-}
+        if (code) {
+          fetch('/get-token', {
+            method: 'POST',
+            body: JSON.stringify({ code }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => response.json())
+          .then(data => {
+            if (data.access_token) {
+              container.innerHTML = `<p>Bem-vindo, seu login foi bem-sucedido! Acesso concedido com o token: ${data.access_token}</p>`;
+            } else {
+              errorMessage.textContent = 'Falha ao obter o token de acesso.';
+            }
+          })
+          .catch(err => {
+            errorMessage.textContent = 'Erro de conexão. Tente novamente mais tarde.';
+          });
+        }
+      })
+      .catch(err => {
+        errorMessage.textContent = 'Erro ao obter as configurações de OAuth.';
+      });
